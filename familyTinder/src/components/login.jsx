d0e0575a3ef527a71a754fefc292aa3bbace077e
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-06-23 23:48:19 +0530

    Made login form center

--- familyTinder/src/components/login.jsx
+++ familyTinder/src/components/login.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <div className="card-body items-center text-center">
+      <div className="card flex items-center justify-center mt-[50px]">
+        <div className="card-body bg-base-150 w-150 h-[64vh] shadow-sm items-center text-center">
           <h2 className="card-title">Login Here</h2>
-          <input type="text" placeholder="email" />
-          <input type="text" placeholder="password" />
+          <div className="flex flex-col p-2 m-5">
+            <input type="text" placeholder="email" className="p-2 m-3 border-none rounded-lg w-[100%]" />
+            <input type="text" placeholder="password"  className="p-2 m-3 border-solid border-inherit rounded-lg w-[100%]" />
+          </div>
           <div className="card-actions">
             <button className="btn btn-primary">Login</button>
           </div>
