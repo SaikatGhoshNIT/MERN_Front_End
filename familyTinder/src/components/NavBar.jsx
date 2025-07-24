21f9faed0fdd38ca491aba2d0498afec18e0c906
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-07-24 23:56:25 +0530

    Showing Name and image from the actual User data.

--- familyTinder/src/components/NavBar.jsx
+++ familyTinder/src/components/NavBar.jsx
@@ -9,9 +9,9 @@ const NavBar = () => {
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">Family Tinder</a>
         </div>
+        <span>{user?.user?.firstName}</span>
         <div className="flex gap-2 mr-5">
           {user && <div className="dropdown dropdown-end">
-            <span>{user.firstName}</span>
             <div
               tabIndex={0}
               role="button"
@@ -24,6 +24,23 @@ const NavBar = () => {
                 />
               </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>}
         </div>
       </div>
