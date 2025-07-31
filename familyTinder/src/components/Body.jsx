50a318ff2e9a45d66aa6846c7ff5528a022c3ec2
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-07-31 21:40:07 +0530

    Corrected the Error element rendering with useState

--- familyTinder/src/components/Body.jsx
+++ familyTinder/src/components/Body.jsx
@@ -9,10 +9,12 @@ import axios from 'axios'
 import { BASE_URL } from '../../utils/constants' // Import the base URL from constants
 import { useNavigate } from 'react-router-dom'
 import Error from './Error';
+import { useState } from 'react';
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [hasError, setHasError] = useState(false);
 
   const fetchUser = async () => {
     try{
@@ -29,7 +31,7 @@ const Body = () => {
         navigate("/login");
       }
       else{
-        return(<Error/>); // Render an error component if the request fails
+        setHasError(true) // Render an error component if the request fails
       }
       console.error("Error fetching user data:", error);
     }
@@ -41,6 +43,8 @@ const Body = () => {
 
 
   return (
+    hasError ? <Error /> :
+    // Render the main layout with NavBar, Outlet, and Footer
     <div className='flex flex-col min-h-screen'>
         <NavBar/>
         <Outlet/>
