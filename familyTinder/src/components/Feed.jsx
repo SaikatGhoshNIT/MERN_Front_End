af2aa674cbad161fb0d683e10695a354a8967f7e
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-08-10 20:27:12 +0530

    Handeling login from Feed page, added a button with condition, if user not logged in then to show a button on feed page to navigate to login page. And if user already logged in then the button should not be visible on Feed page.

--- familyTinder/src/components/Feed.jsx
+++ familyTinder/src/components/Feed.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const Feed = () => {
-  return (
+  const [isVisible, setIsVisible] = useState(true);
+  const user = useSelector((state) => state.user); // Assuming you have a user state in Redux
+
+  
+  useEffect(() => {
+  if (user === null || Object.keys(user).length === 0) {
+    setIsVisible(true); // Show button when user is not logged in
+  } else {
+    setIsVisible(false); // Hide button when user is logged in
+  }
+}, [user]);
+  
+
+
+  return (<>
     <div>Feed</div>
+    <Link to="/login">
+      <button className={`p-[3px] m-0.5 text-center bg-emerald-400 ${isVisible === false ? 'hidden' : ''}`}>Login/Sign-in</button>
+    </Link>
+    </>
   )
 }
 
-export default Feed
+export default Feed;
